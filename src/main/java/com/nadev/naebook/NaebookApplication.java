ce073187688da03a92cecacc9db7b974f438dd8b
bomin <bomin_93@naver.com>
2021-02-13 21:38:25 +0900

    Initial commit

---
+++ src/main/java/com/nadev/naebook/NaebookApplication.java
@@ -0,0 +1,13 @@
+package com.nadev.naebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaebookApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NaebookApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/nadev/naebook/NaebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadev.naebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaebookApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
