6b5dc506cc3c8af4202f9fc97e681adc46a352a0
bomin <bomin_93@naver.com>
2021-02-20 18:53:02 +0900

    feat(follow): 팔로우 기능 구현

--- src/main/java/com/nadev/naebook/api/UserController.java
+++ src/main/java/com/nadev/naebook/api/UserController.java
@@ -6,12 +6,16 @@ import com.nadev.naebook.domain.user.User;
 import com.nadev.naebook.dto.ProfileRequestDto;
 import com.nadev.naebook.exception.UserNotFoundException;
 import com.nadev.naebook.service.UserService;
+import javax.validation.constraints.NotEmpty;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +42,12 @@ public class UserController {
       throw new UserNotFoundException();
     }
     return user;
+  }
+
+  @PostMapping("/follow")
+  public ResponseEntity follow(@LoginUser SessionUser user, @RequestBody @NotEmpty String email) {
+    userService.follow(user.getEmail(), email);
+    return ResponseEntity.status(HttpStatus.OK).build();
   }
 
   @Data
---
+++ src/main/java/com/nadev/naebook/repository/RelationRepository.java
@@ -0,0 +1,8 @@
+package com.nadev.naebook.repository;
+
+import com.nadev.naebook.domain.Relation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RelationRepository extends JpaRepository<Relation, Long> {
+
+}
