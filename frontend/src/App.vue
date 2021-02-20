9aec13815c63a33d744f2f12142ea51019e1dcca
bomee.yoon <bomee.yoon@godo.co.kr>
2021-02-20 23:41:48 +0900

    feat(main) 메인화면 바디 마크업 완료

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,159 +1,7 @@
 <template>
   <div>
 	  <content-header />
-	  <main id="main">
-		  <section id="recommend">
-			  <div class="content-wrap pd-side-large">
-				  <div class="title-wrap vertical-align">
-					  <h1>오늘의 추천</h1>
-				  </div>
-				  <div class="main-content-wrap row">
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="btn-wrap vertical-align">
-					  <button class="view-more">더보기</button>
-				  </div>
-			  </div>
-		  </section>
-		  <section id="popular" class="line-large">
-			  <div class="content-wrap pd-side-large">
-				  <div class="title-wrap vertical-align">
-					  <h1>인기 책</h1>
-				  </div>
-				  <div class="main-content-wrap row">
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div>
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="btn-wrap vertical-align">
-					  <button class="view-more">더보기</button>
-				  </div>
-			  </div>
-		  </section>
-		  <section id="review" class="line-large">
-			  <div class="content-wrap pd-side-large">
-				  <div class="title-wrap vertical-align">
-					  <h1>신규 리뷰</h1>
-				  </div>
-				  <div class="main-content-wrap column">
-					  <div class="review-wrap">
-						  <div>
-							  <div class="row">
-								  <span class="thumbnail"><span class="iconify thumbnail-icon" data-icon="bi:person-fill" data-inline="false"></span></span>
-								  <div class="text-wrap pd-left-regular pd-top-regular">
-									  <h1>함수형 자바스크립트</h1>
-									  <p>마이클 포거스</p>
-								  </div>
-							  </div>
-							  <div class="pd-left-regular">
-								  <div class="review-desc-wrap">
-									  <span class="iconify" data-icon="fluent:text-quote-24-filled" data-inline="false"></span>
-									  <p>함수형 자바스크립트를 읽으면, 함수형 프로그래밍을 잘 할 수 있을까 그렇다면 백번이라도 읽겠어</p>
-								  </div>
-							  </div>
-							  <span class="rating-wrap pd-left-regular"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-						  <div>
-							  <img class="img-large" src="" alt="">
-						  </div>
-						  <!--						<img class="img-large" src="" alt="">-->
-						  <!--						<div class="text-wrap">-->
-						  <!--							<h1>어린왕자</h1>-->
-						  <!--							<p>앙투안 드 생택쥐페리</p>-->
-						  <!--							<span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>-->
-						  <!--						</div>-->
-					  </div>
-					  <div class="review-wrap">
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div class="review-wrap">
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-					  <div class="review-wrap">
-						  <img class="img-large" src="" alt="">
-						  <div class="text-wrap">
-							  <h1>어린왕자</h1>
-							  <p>앙투안 드 생택쥐페리</p>
-							  <span class="rating-wrap"><span class="rating">★ ★ ★ ★ ★</span></span>
-						  </div>
-					  </div>
-				  </div>
-				  <div class="btn-wrap vertical-align">
-					  <button class="view-more">더보기</button>
-				  </div>
-			  </div>
-		  </section>
-	  </main>
+	  <body-content />
 	  <footer id="footer"></footer>
   </div>
 </template>
@@ -161,8 +9,9 @@
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
 import ContentHeader from "@/views/main/header/TopContent.vue";
+import BodyContent from "@/views/main/body/BodyContent.vue";
 @Component({
-	components: {ContentHeader}
+	components: {BodyContent, ContentHeader}
 })
 export default class App extends Vue {
 }
