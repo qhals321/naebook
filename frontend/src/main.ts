8efb1adcc43afc9a542332ac3c2e01f69d90ed6f
bomee.yoon <bomee.yoon@godo.co.kr>
2021-02-20 23:39:25 +0900

    feat(header) 메인화면 헤더 마크업 완료

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import '@/assets/css/root.css';
+import '@/assets/css/main/main.css';
+
 Vue.config.productionTip = false;
 
 new Vue({
