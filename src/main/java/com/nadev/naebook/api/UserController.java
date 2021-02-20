3f83b12f616c718ea5c24bd7771a0d9ba2b4e618
bomin <bomin_93@naver.com>
2021-02-20 18:55:05 +0900

    fix(crossOrigi)

--- src/main/java/com/nadev/naebook/api/UserController.java
+++ src/main/java/com/nadev/naebook/api/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
+@CrossOrigin
 public class UserController {
 
   private final UserService userService;
