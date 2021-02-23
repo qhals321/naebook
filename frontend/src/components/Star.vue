88e84b9f18eaa681a80c2f391c1a383037875f38
bomee.yoon <bomee.yoon@godo.co.kr>
2021-02-23 21:41:05 +0900

    별 평점 기능 구현

---
+++ frontend/src/components/Star.vue
@@ -0,0 +1,75 @@
+<template>
+  <div
+    class="outline"
+    @mouseenter="mouseMove('fill')"
+    @mouseleave="mouseMove('outline')"
+    @click="selectRating"
+  >
+    <span>
+      <span
+        class="iconify star"
+        data-icon="ant-design:star-outlined"
+        data-inline="false"
+      ></span>
+    </span>
+    <span :class="['fill', { hide: !colorable }]">
+      <span
+        class="iconify star"
+        data-icon="ant-design:star-filled"
+        data-inline="false"
+      ></span>
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component, Prop, PropSync } from 'vue-property-decorator';
+  import { StarStyleType } from '@/types/common';
+  @Component
+  export default class Star extends Vue {
+    @PropSync('currentRating')
+    private currentRatingSync!: number;
+    @Prop()
+    private readonly uniqueKey!: number;
+    @Prop()
+    private readonly selectedRating!: number;
+
+    private mouseMove(type: StarStyleType) {
+      type === 'fill' ? this.setCurrentRating() : this.resetRating();
+    }
+
+    private setCurrentRating() {
+      this.currentRatingSync = this.uniqueKey;
+    }
+
+    private resetRating() {
+      this.currentRatingSync = this.selectedRating;
+    }
+
+    private get colorable(): boolean {
+      return (
+        this.currentRatingSync !== 0 && this.currentRatingSync >= this.uniqueKey
+      );
+    }
+
+    private selectRating() {
+      this.$emit('select-rating');
+    }
+  }
+</script>
+<style scoped>
+  >>> .star > path {
+    color: gold;
+  }
+  .outline {
+    display: inline-block;
+    position: relative;
+  }
+  .fill {
+    position: absolute;
+    left: 0;
+  }
+  .hide {
+    display: none;
+  }
+</style>
---
+++ frontend/src/components/StarRating.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <template v-for="idx in 5">
+      <star
+        :key="idx"
+        :current-rating.sync="currentRating"
+        :unique-key="idx"
+        :selected-rating="selectedRating"
+        @select-rating="selectRating"
+      />
+    </template>
+  </div>
+</template>
+
+<script lang="ts">
+  import { Vue, Component } from 'vue-property-decorator';
+  import Star from '@/components/Star.vue';
+  @Component({
+    components: { Star },
+  })
+  export default class StarRating extends Vue {
+    private currentRating = 0;
+    private selectedRating = 0;
+
+    private selectRating() {
+      this.selectedRating = this.currentRating;
+    }
+  }
+</script>
