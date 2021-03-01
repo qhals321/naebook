62f903292c5ebc4c8715c78011385be0b51fe1a9
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-01 23:25:54 +0900

    :art: 네비게이션 마크업 구조 및 css 변경

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="ly-container">
     <content-header />
+    <content-nav />
     <body-content />
     <footer id="footer"></footer>
   </div>
@@ -10,8 +11,9 @@
   import { Vue, Component } from 'vue-property-decorator';
   import ContentHeader from '@/views/main/header/TopContent.vue';
   import BodyContent from '@/views/main/body/BodyContent.vue';
+  import ContentNav from '@/views/main/nav/ContentNav.vue';
   @Component({
-    components: { BodyContent, ContentHeader },
+    components: { ContentNav, BodyContent, ContentHeader },
   })
   export default class App extends Vue {}
 </script>
