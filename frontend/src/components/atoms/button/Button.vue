4074e9a1d5b4670587f3a792090517f91b5cc98c
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-10 22:54:11 +0900

    :seedling: 초기 파일구조 세팅

---
+++ frontend/src/components/atoms/button/Button.vue
@@ -0,0 +1,12 @@
+<template>
+  <button>test button</button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Button",
+  components: {}
+});
+</script>
