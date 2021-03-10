c7db89e0f35f9cf9e457687d6c89f329c5426dd7
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-10 22:23:30 +0900

    :wrench: 포트 및 서버 연결 환경 설정 vue.config.js

---
+++ frontend/vue.config.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
+
+module.exports = {
+  outputDir: path.resolve(__dirname, "../src/main/resources/static/"),
+  devServer: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:9000",
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+};
