7c8ef8ab4ad5ad8c0d0d94e2d93008a7833f371e
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-15 20:30:05 +0900

    :lipstick: root.css 스타일 추가

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import '@/assets/css/root.css';
+import '@/assets/css/index.css';
+
 createApp(App)
   .use(store)
   .use(router)
