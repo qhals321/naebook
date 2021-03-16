49ea82bd734c9299bf359ed0d60416460dd41236
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-16 23:14:50 +0900

    :construction: Link atom 추가

---
+++ frontend/src/components/atoms/link/Link.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-link :to="linkToOption">{{ linkToOption.name }}</router-link>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Link',
+    props: ['linkToOption'],
+  });
+</script>
