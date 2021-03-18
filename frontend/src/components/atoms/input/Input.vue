3509c210361999ddd3eb537b0d5ea0ad29305264
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-18 21:31:28 +0900

    :construction: Add Input and Label atoms

---
+++ frontend/src/components/atoms/input/Input.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    :id="option.id"
+    :class="option.class"
+    :type="option.type"
+    :placeholder="option.placeholder"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Input',
+    props: ['option'],
+  });
+</script>
+
+<style scoped></style>
---
+++ frontend/src/components/atoms/label/Label.vue
@@ -0,0 +1,14 @@
+<template>
+  <label :for="option.for">{{ option.label }}</label>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Label',
+    props: ['option'],
+  });
+</script>
+
+<style scoped></style>
