42468f52499f5a8d3cb140fa8f0dd8e843eab3f5
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-21 00:47:18 +0900

    :construction: Label atom 추가

---
+++ frontend/src/components/atoms/label/Label.vue
@@ -0,0 +1,28 @@
+<template>
+  <label :for="labelOption.for">{{ option.label }}</label>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  const option = {
+    for: '',
+    label: '',
+  };
+  export default defineComponent({
+    name: 'Label',
+    props: {
+      labelOption: {
+        required: true,
+      },
+    },
+    setup(props) {
+      return {
+        option: {
+          ...option,
+          ...props.labelOption,
+        },
+      };
+    },
+  });
+</script>
