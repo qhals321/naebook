7b4cd9f11468411fe07588021ca86cb7ba65bb9c
KwonYG <andamirocll@gmail.com>
2021-03-20 20:39:13 +0900

    feat: 아이콘 컴포넌트 추가

---
+++ frontend/src/components/atoms/Icon/AIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <Icon :icon="icons[iconName]" />
+</template>
+<script lang="ts">
+  import { Icon } from '@iconify/vue';
+  import baselineClear from '@iconify/icons-ic/baseline-clear';
+
+  export default {
+    name: 'AIcon', // @iconify/vue의 Icon 컴포넌트와 네이밍이 겹처 AIcon이라고 지음
+    components: {
+      Icon,
+    },
+    props: {
+      iconName: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        // https://iconify.design/icon-sets/ic/
+        icons: {
+          baselineClear,
+        },
+      };
+    },
+  };
+</script>
