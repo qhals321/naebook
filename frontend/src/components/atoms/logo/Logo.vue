7cc3a00035b6cf5357c2481a14cfb2754b6284f0
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-21 01:03:49 +0900

    :construction: Logo atom 추가

---
+++ frontend/src/components/atoms/logo/Logo.vue
@@ -0,0 +1,26 @@
+<template>
+  <Link :option="logoOption" />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import Link from '@/components/atoms/link/Link.vue';
+  import { LinkOption } from '@/types/atoms';
+
+  const logoOption: LinkOption = {
+    type: 'link',
+    class: 'logo',
+    path: '/',
+    label: 'NaeBook',
+  };
+
+  export default defineComponent({
+    name: 'Logo',
+    components: { Link },
+    setup() {
+      return {
+        logoOption,
+      };
+    },
+  });
+</script>
