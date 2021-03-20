cdc77bb25c415abcf8e7c7909dc35acc9fd7b8a4
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-21 00:40:14 +0900

    :construction: Input atom 추가

---
+++ frontend/src/components/atoms/input/Input.vue
@@ -0,0 +1,43 @@
+<template>
+  <input
+    :id="option.id"
+    :class="option.class"
+    :type="option.type"
+    :placeholder="option.placeholder"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  const option = {
+    id: '',
+    class: 'white-regular',
+    type: 'text',
+    placeholder: '검색어를 입력하세요',
+  };
+
+  export default defineComponent({
+    name: 'Input',
+    props: ['inputOption'],
+    setup(props) {
+      return {
+        option: {
+          ...option,
+          ...props.inputOption,
+        },
+      };
+    },
+  });
+</script>
+
+<style scoped>
+  .white-regular {
+    height: 40px;
+    width: 290px;
+    padding: 10px 10px 10px 20px;
+    background-color: var(--color-white);
+    border: 1px solid var(--color-white);
+    color: var(--color-dark-gray);
+  }
+</style>
