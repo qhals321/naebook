34dadef23e7ff47e79dc4f24ad153e1678abec62
KwonYG <andamirocll@gmail.com>
2021-03-20 20:01:32 +0900

    feat: 태그 컴포넌트 추가

---
+++ frontend/src/components/atoms/Tag/Tag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>{{ text }}</div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    name: 'Tag',
+    props: {
+      text: {
+        type: String,
+        required: true,
+      },
+    },
+
+    setup(props) {
+      const { text } = props;
+      return {
+        text,
+      };
+    },
+  });
+</script>
