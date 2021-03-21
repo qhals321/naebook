b063fe62a21a29255c764ceac6bbf6a6e43df929
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-21 22:06:33 +0900

    :construction: Icon atoms 추가

---
+++ frontend/src/components/atoms/Icon/IconComponent.vue
@@ -0,0 +1,54 @@
+<template>
+  <inline-icon
+    :class="[option.color, option.size]"
+    :icon="icons[option.name]"
+  />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { InlineIcon } from '@iconify/vue';
+  import notificationNew from '@iconify-icons/carbon/notification-new';
+  import notification from '@iconify-icons/carbon/notification';
+  import profile from '@iconify-icons/bi/person-circle';
+
+  const option = {
+    color: 'navy',
+    size: 'regular',
+  };
+
+  export default defineComponent({
+    name: 'IconComponent',
+    components: { InlineIcon },
+    props: ['iconOption'],
+    data() {
+      return {
+        icons: {
+          notificationNew,
+          notification,
+          profile,
+        },
+      };
+    },
+    setup(props) {
+      return {
+        option: {
+          ...option,
+          ...props.iconOption,
+        },
+      };
+    },
+  });
+</script>
+
+<style scoped>
+  .regular {
+    font-size: 1.5rem;
+  }
+  .navy {
+    color: var(--color-navy);
+  }
+  .red {
+    color: crimson;
+  }
+</style>
--- frontend/src/views/pages/nav/ContentNav.vue
+++ frontend/src/views/pages/nav/ContentNav.vue
@@ -1,15 +1,21 @@
 <template>
   <!--	TODO: 화면 확인을 위한 임시 코드  -->
-  <nav><navigator :link-options="menuLinkOptions" /></nav>
+  <nav>
+    <icon-component :icon-option="{ name: 'notificationNew', color: 'red' }" />
+    <icon-component :icon-option="{ name: 'notification' }" />
+    <icon-component :icon-option="{ name: 'profile' }" />
+    <navigator :link-options="menuLinkOptions" />
+  </nav>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
   import Navigator from '@/components/molecules/Navigator.vue';
+  import IconComponent from '@/components/atoms/Icon/IconComponent.vue';
   import { LinkOption } from '@/types/atoms';
   export default defineComponent({
     name: 'ContentNav',
-    components: { Navigator },
+    components: { Navigator, IconComponent },
     setup() {
       const menuLinkOptions: LinkOption[] = [
         { path: '/', label: '추천' },
