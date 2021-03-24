d799e941a4a342cfe6513e0e6ffbb811a8dc2518
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-24 22:33:04 +0900

    :lipstick: 헤더 스타일 추가

--- frontend/src/views/pages/header/ContentHeader.vue
+++ frontend/src/views/pages/header/ContentHeader.vue
@@ -1,14 +1,44 @@
 <template>
-  <header>header</header>
+  <header>
+    <div class="header-container">
+      <logo />
+      <search-keyword class="search-keyword" :use-clear-icon="true" />
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-
+  import SearchKeyword from '@/components/molecules/Search/SearchKeyword.vue';
+  import Logo from '@/components/atoms/Logo/Logo.vue';
   export default defineComponent({
     name: 'ContentHeader',
-    components: {},
+    components: { Logo, SearchKeyword },
   });
 </script>
 
-<style scoped></style>
+<style scoped>
+  header {
+    margin: 0 auto;
+    background-color: var(--color-navy);
+    height: 8rem;
+    min-width: 474px;
+    max-width: 1440px;
+  }
+
+  .header-container {
+    width: 100%;
+    height: 100%;
+    display: inline-grid;
+    grid-column: 3;
+    justify-items: center;
+    align-items: center;
+    grid-row: 1;
+    grid-template-columns: 12rem 1fr 20rem;
+    padding: 0 32px;
+  }
+  .search-keyword {
+    grid-column: 3/4;
+    height: 32px;
+  }
+</style>
