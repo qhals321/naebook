4b2cb8aa04ad5027016228ba4edddbefecaf812a
bomee.yoon <bomee.yoon@godo.co.kr>
2021-03-24 22:36:40 +0900

    :recycle: Select atom 리팩토링

--- frontend/src/components/atoms/Select/Select.vue
+++ frontend/src/components/atoms/Select/Select.vue
@@ -1,16 +1,36 @@
 <template>
   <select>
-    <slot></slot>
+    <option v-for="(option, idx) in options" :key="idx" :value="option.value">{{
+      option.text
+    }}</option>
   </select>
 </template>
 
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, PropType } from 'vue';
 
   export default defineComponent({
     name: 'Select',
-    components: {},
+    props: {
+      options: {
+        type: Object as PropType<{ text: string; value: string }[]>,
+        required: true,
+      },
+    },
   });
 </script>
 
-<style scoped></style>
+<style scoped>
+  select {
+    min-width: 70px;
+    cursor: pointer;
+    font-family: var(--font-style-poor);
+    font-size: var(--font-text-small);
+  }
+
+  option {
+    /* TODO: css 안 먹힘. */
+    font-family: var(--font-style-poor);
+    font-size: var(--font-text-small);
+  }
+</style>
